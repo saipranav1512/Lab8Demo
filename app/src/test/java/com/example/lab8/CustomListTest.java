c3516d254294e4a12ef9f0e1e38051cf1e7c1ed1
saipranav1512 <145928964+saipranav1512@users.noreply.github.com>
2024-03-08 02:21:44 -0700

    Update CustomListTest.java

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -21,17 +21,13 @@ public class CustomListTest {
         return list;
     }
 
-    /**
-     *get the size of the list
-     *increase the list by adding a new city
-     *check if our current size matches the initial size plus one
-     */
-    @Test
-    public void addCityTest(){
-        list = MockCityList();
-        int listSize = list.getCount();
-        list.addCity(new City("Estevan", "SK"));
-        assertEquals(list.getCount(),listSize+1);
+    /** 
+     *this adds a city object to the list 
+     *the second phase,you can add the city
+     *@paramcity 
+     */ 
+    public void addCity(Citycity){ 
+        cities.add(city); 
     }
 
 }
